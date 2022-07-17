7b0ffdcf23bd2c6409788d69ed6dbff70cbdfb03
kseniadumpling <ksenia.dumpling@gmail.com>
2022-07-17 20:10:57 +0300

    Add simple slack event API handler

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
 	"github.com/slack-go/slack/socketmode"
 )
 
@@ -27,6 +29,43 @@ func main() {
 		// Some custom logger
 		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
 	)
+
+	// gotoutine context
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// setup defer for cleaning up resources at the end
+	defer cancel()
+
+	go func(ctx context.Context, client *slack.Client, socketClient *socketmode.Client) {
+		for {
+			select {
+			case <-ctx.Done():
+				log.Println("Shutting down socketmode listener")
+				return
+
+			case event := <-socketClient.Events:
+				switch event.Type {
+				case socketmode.EventTypeConnecting:
+					log.Println("Connecting to Slack with Socket Mode...")
+
+				case socketmode.EventTypeConnectionError:
+					log.Println("Connection failed. Retrying later...")
+
+				case socketmode.EventTypeConnected:
+					log.Println("Connected to Slack with Socket Mode.")
+
+				case socketmode.EventTypeEventsAPI:
+					eventsAPIEvent, ok := event.Data.(slackevents.EventsAPIEvent)
+					if !ok {
+						log.Printf("Could not type cast the event to the EventsAPIEvent: %v\n", event)
+						continue
+					}
+					socketClient.Ack(*event.Request)
+					log.Println(eventsAPIEvent)
+				}
+			}
+		}
+	}(ctx, client, socketClient)
 
 	socketClient.Run()
 }
